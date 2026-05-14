76be9a008d5a8760053ddd94f719834a7a902f58
Tanish S K <Tanishcse22@gmail.com>
2026-05-14 11:26:44 +0530

    trouble shoot31

--- backend/server.py
+++ backend/server.py
@@ -135,11 +135,10 @@ def job_complete(job_id):
     if (cnt == 0):
         try: 
             response = httpx.post(f"{SERVER}/merge/{job_id}",timeout=None)
-            return response
         except: return {"message": "http merge requestn wrong"}
         
         try: output_file = response.json()["output_file"]
-        except: return {"message": response}
+        except: return {"message": response.json()}
 
         try: content = open(output_file,"rb").read()
         except: return {"message": "output file reading"}
